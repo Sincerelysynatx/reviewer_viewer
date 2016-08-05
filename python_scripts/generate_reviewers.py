72e76a0c850f6d72336545285ef0c3e5750d667c
Sean Pimentel <sean.pat.pimentel@hpe.com>
2016-08-04 17:31:01 -0700

    Version 1.0 is done and ready for users to use
    Signed-off-by: Sean Pimentel <sean.pat.pimentel@hpe.com>

--- python_scripts/generate_reviewers.py
+++ python_scripts/generate_reviewers.py
@@ -24,18 +24,26 @@ for i in range(0, array_len - 1):
         temp_array_of_read_emails = [f.replace("\n", "") for f in temp_array_of_read_emails]
         names = []
         for email in temp_array_of_read_emails:
-            cmd = "ypcat passwd | grep " + email + " | awk -F: '{ print $5}' | awk -F, '{ print $1}'"
+            cmd = "ypcat passwd | grep " + email + " | awk -F: '{print $5}' | awk -F, '{print $1}'"
             p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out = p.communicate()[0]
             out.rstrip()
-            names.append("Name: " + out + " email: " + email + "\n\n")
+            if out == "":
+                out = "ERROR WITH LOOKUP IN YPCAT\n"
+            names.append("Name: " + out + "Email: " + email)
         array_names.append(names)
         file.close()
 
-# Format the string for 
+# Format the string for display
 array_files = [f.replace('../../', '') for f in array_files]
 array_files = [f.replace('halon-src/', '') for f in array_files]
 array_files = [f.replace('REVIEWERS', '') for f in array_files]
+array_files = [f.replace('halon/', '') for f in array_files]
+array_files = [f.replace('/', '') for f in array_files]
+
+for n, i in enumerate(array_files):
+    if i == "":
+        array_files[n] = "halon"
 
 dictionary = dict(zip(array_files, array_names))
 
@@ -43,34 +51,10 @@ sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(0))
 
 html_output = ""
 
-size_of_cols = (array_len - 1) / 4
-
-remainder = (array_len - 1) % 4
-
-for i in range(0, 3):
-    html_output += """<div id="col_{}" class="w3-third">""".format(i) + "\n"
-    for j in range(0, size_of_cols):
-        current = sorted_dictionary.pop()
-        html_output += """<div id="{} {}" class="">
-    <div class="w3-card-8 w3-margin">
-        <h3 class="w3-green w3-center">{}</h3>
-        <div class="w3-padding">
-            <p style="white-space: pre-line">{}</p>
-        </div>
-    </div>
-    </div>""".format(module, "".join(members), module, "".join(members)) + "\n"
-    if (remainder > 0):
-       html_output += """    <div id="item_remainder"></div>\n"""
-       remainder -= 1
-    html_output += """</div>\n"""
-
-index = 0
-
-#for module, members in sorted_dictionary:
-#    html_output += """<div id="{} {}" class="w3-third">
-#    <div class="w3-card-8 w3-margin card"><h2 class="w3-green w3-center w3-padding">{}</h2>
-#    <div class="w3-padding"><p class="w3-large" style="white-space: pre-line">{}</p></div></div>
-#    </div>""".format(module, "".join(members), module, "".join(members)) + "\n"
-#    index += 1
+for module, members in sorted_dictionary:
+    html_output += """<div id="{} {}" class="grid-item">
+    <div class="w3-card-8 w3-margin card"><h2 class="w3-center w3-text-white w3-padding" style="background-color: #5F7A76;">{}</h2>
+    <div class="w3-padding"><p class="w3-large" style="white-space: pre-line">{}</p></div></div>
+    </div>""".format(module, "".join(members).lower(), module, "\n\n".join(members)) + "\n"
 
 print(html_output)
